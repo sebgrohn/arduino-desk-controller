8f1b9e16be5a1e5494015128d00efaf884a89fe5
Sebastian Gröhn <sebastian@herakles.sebgrohn.se>
2015-10-16 19:48:43 +0200

    Adjust configuration to match new desk

--- Config.h
+++ Config.h
@@ -4,22 +4,22 @@
 #include <Arduino.h>
 
 
-const double minHeight   = 0.685;  // in meters
-const double maxHeight   = 1.175;  // in meters
+const double minHeight   = 0.690;  // in meters
+const double maxHeight   = 1.175;  // in meters  // TODO measure it
 const double sitHeight   = 0.700;  // in meters
 const double standHeight = 1.110;  // in meters
 
-const double upSpeed   = 0.0295;  // in m/s
-const double downSpeed = 0.0335;  // in m/s
+const double upSpeed   = 0.02050;  // in m/s
+const double downSpeed = 0.02300;  // in m/s
 
 const String minPosition   = "Min   ";
 const String maxPosition   = "Max   ";
 const String sitPosition   = "Sit   ";
 const String standPosition = "Stand ";
 
-const char enableInputPin = 4;
-const char upInputPin     = 3;
-const char downInputPin   = 2;
+const char enableInputPin = 0;
+const char upInputPin     = 5;
+const char downInputPin   = 4;
 
 const char lcdRSPin      = 8;
 const char lcdEnablePin  = 9;
