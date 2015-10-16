d576959376f0928636c3646e105465f7bb1930d4
Sebastian Gröhn <sebastian@herakles.sebgrohn.se>
2015-10-16 19:29:16 +0200

    Add experimental LiquidCrystalWindow

--- DeskControllerApp.ino
+++ DeskControllerApp.ino
@@ -14,3 +14,52 @@
 #include <utility.h>
 #include <map>
 
+// ---------------------------------------
+
+struct LiquidCrystalWindowParams {
+  
+  int x;
+  int y;
+  
+  int width;
+  int height;
+  
+  LiquidCrystalWindowParams();
+  
+  LiquidCrystalWindowParams(const int& x, const int& y, const int& width, const int& height);
+};
+
+
+class LiquidCrystalWindow : public Print {
+public:
+  const LiquidCrystalWindowParams params;
+  
+  LiquidCrystal * const lcd;
+  
+  LiquidCrystalWindow(LiquidCrystal * const & lcd, const LiquidCrystalWindowParams& params);
+  
+  virtual size_t write(uint8_t);
+};
+
+
+
+LiquidCrystalWindowParams::LiquidCrystalWindowParams() {}
+
+LiquidCrystalWindowParams:: LiquidCrystalWindowParams(const int& x, const int& y, const int& width, const int& height)
+    : x(x),
+      y(y),
+      width(width),
+      height(height) {
+}
+
+
+LiquidCrystalWindow::LiquidCrystalWindow(LiquidCrystal * const & lcd, const LiquidCrystalWindowParams& params)
+    : lcd(lcd),
+      params(params) {
+}
+
+size_t LiquidCrystalWindow::write(uint8_t value) {
+  lcd->setCursor(params.x, params.y);
+  return lcd->write(value);
+}
+
